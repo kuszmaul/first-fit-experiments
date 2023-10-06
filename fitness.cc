4bc4bff896fc5b6b89047a5de0bd63b98260345b
Bradley C. Kuszmaul <kuszmaul@gmail.com>
2023-10-06 14:48:20 -0400

    More tests to see if some allocations work right

--- fitness.cc
+++ fitness.cc
@@ -6,26 +6,28 @@
 class FirstFit;
 
 class Block {
- private:
-  friend FirstFit;
-  Block(size_t start, size_t length) :_start(start), _length(length) {}
  public:
-  friend bool operator<(Block a, Block b);
-  friend std::ostream& operator<<(std::ostream& os, Block b);
+  Block(size_t start, size_t size) :_start(start), _size(size) {}
+  size_t start() const { return _start; }
+  size_t size() const { return _size; }
 
  private:
   size_t _start;
-  // We remember the length associated with an address.
-  size_t _length;
+  size_t _size;
 };
 
 bool operator<(Block a, Block b) {
-  if (a._start == b._start) assert(a._length == b._length);
-  return a._start < b._start;
+  if (a.start() == b.start()) assert(a.size() == b.size());
+  return a.start() < b.start();
+}
+
+bool operator==(Block a, Block b) {
+  if (a.start() == b.start()) assert(a.size() == b.size());
+  return a.start() == b.start();
 }
 
 std::ostream& operator<<(std::ostream& os, Block b) {
-  return os << "{" << b._start << ", " << b._length << "}";
+  return os << "{" << b.start() << ", " << b.size() << "}";
 }
 
 class FirstFit {
@@ -44,19 +46,19 @@ class FirstFit {
 Block FirstFit::Alloc(size_t size) {
   size_t prev_end = 0;
   for (const Block& block : _allocated_blocks) {
-    assert(block._start >= prev_end);
-    if (block._start - prev_end >= size) {
+    assert(block.start() >= prev_end);
+    if (block.start() - prev_end >= size) {
       Block block{prev_end, size};
       _allocated_blocks.insert(block);
       return block;
     }
-    prev_end = block._start + block._length;
+    prev_end = block.start() + block.size();
   }
   size_t end = 0;
   if (!_allocated_blocks.empty()) {
     auto it = _allocated_blocks.end();
     --it;
-    end = it->_start + it->_length;
+    end = it->start() + it->size();
   }
   _high_water = std::max(_high_water, end + size);
   Block block{end, size};
@@ -67,18 +69,42 @@ Block FirstFit::Alloc(size_t size) {
 void FirstFit::Free(Block block) {
   auto it = _allocated_blocks.find(block);
   assert(it != _allocated_blocks.end());
-  assert(it->_start == block._start);
-  assert(it->_length == block._length);
+  assert(*it == block);
   _allocated_blocks.erase(it);
 }
 
-int main() {
+// A simple test.  Do we reuse allocations?
+void Test1() {
   FirstFit ff;
   Block a = ff.Alloc(10);
   std::cout << "Allocated " << a << std::endl;
   ff.Free(a);
-  a = ff.Alloc(10);
+  Block b = ff.Alloc(10);
   std::cout << "Allocated " << a << std::endl;
   std::cout << "High-water = " << ff.get_high_water() << std::endl;
   assert(ff.get_high_water() < 20);
+  assert(a == b);
+}
+
+void Test2() {
+  FirstFit ff;
+  /*Block a = */ff.Alloc(10);
+  Block b = ff.Alloc(15);
+  /*Block c = */ff.Alloc(20);
+  Block d = ff.Alloc(25);
+  /*Block e = */ff.Alloc(30);
+  ff.Free(b);
+  ff.Free(d);
+  Block f = ff.Alloc(21);
+  assert(f.start() == 10 + 15 + 20);
+  Block g = ff.Alloc(14);
+  assert(g.start() == 10);
+  Block h = ff.Alloc(2);
+  assert(h.start() == 10 + 15 + 20 + 21);
+  assert(ff.get_high_water() == 10 + 15 + 20 + 25 + 30);
+}
+
+int main() {
+  Test1();
+  Test2();
 }
